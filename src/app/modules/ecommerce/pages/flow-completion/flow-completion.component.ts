5e62c539c07c0c9d6eb97bd2f9b9d7f56549a720
Jorge Chourio <jorge_chourio@hotmail.com>
2022-04-30 05:44:09 -0400

    Fixed ',' format in notifications total in flow-completion

--- src/app/modules/ecommerce/pages/flow-completion/flow-completion.component.ts
+++ src/app/modules/ecommerce/pages/flow-completion/flow-completion.component.ts
@@ -844,15 +844,16 @@ export class FlowCompletionComponent implements OnInit {
       (a, b) => a + b.amount,
       0
     );
-    const fullLink = 'https://kanddys.com/ecommerce/order-info/' + this.orderId;
+    const fullLink = `${environment.uri}/ecommerce/order-info/${this.orderId}`;
+    const ammount = new Intl.NumberFormat('es-MX').format(this.ammount.value.toLocaleString('es-MX'));
     if (this.fakeData.items[0].customizer)
       this.whatsappLink = `https://wa.me/${
         this.merchantInfo.owner.phone
       }?text=Hola%20${
         this.merchantInfo.name
       },%20le%20acabo%20de%20hacer%20un%20pago%20de%20$${
-        this.ammount ??
-        Math.round((totalPrice * 1.18 + Number.EPSILON) * 100) / 100
+        (ammount && ammount != '0') ? ammount :
+        (Math.round((totalPrice * 1.18 + Number.EPSILON) * 100) / 100)
       }.%20Mi%20nombre%20es:%20${
         this.userData.name
       }.%20Mas%20info%20aquí%20${fullLink}`;
@@ -862,7 +863,7 @@ export class FlowCompletionComponent implements OnInit {
       }?text=Hola%20${
         this.merchantInfo.name
       },%20le%20acabo%20de%20hacer%20un%20pago%20de%20$${
-        this.ammount ?? totalPrice
+        (ammount && ammount != '0') ? ammount : totalPrice.toLocaleString('es-MX')
       }.%20Mi%20nombre%20es:%20${
         this.userData.name
       }.%20Mas%20info%20aquí%20${fullLink}`;
