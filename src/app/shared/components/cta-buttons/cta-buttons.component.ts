2062d4e8504c0c847b071f8a4e1f0dc14a89a61c
JoseBastidas-Prog <85695084+JoseBastidas-Prog@users.noreply.github.com>
2022-05-19 11:33:54 -0400

    cta-buttons update
    Cta-buttons style modification and mode addition on component

--- src/app/shared/components/cta-buttons/cta-buttons.component.ts
+++ src/app/shared/components/cta-buttons/cta-buttons.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-cta-buttons',
@@ -6,11 +7,24 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./cta-buttons.component.scss']
 })
 export class CtaButtonsComponent implements OnInit {
+    
+  imageFolder: string; 
 
-  @Input() btnColor: string = '#40772F';
-  @Input() ctaText: string = '';
+  @Input() mode: string = 'basic' || 'white' //Modos del boton
+    
+  @Input() btnColor: string = '#40772F'; //Basic
+  @Input() ctaText: string = ''; //Basic
 
-  constructor() { }
+  @Input() borderc: string = '#2F6ED8'; //white
+  @Input() borderw: string = '1px' //white
+  @Input() fontColor: string = '#4773D8'; //white
+  @Input() circle: boolean = false; //white
+  @Input() icon: string; //white
+
+
+  constructor() { 
+      this.imageFolder = environment.assetsUrl;
+  }
 
   ngOnInit(): void {
   }
