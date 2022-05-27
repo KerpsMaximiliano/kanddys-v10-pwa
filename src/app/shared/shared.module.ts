9535441452218bd46ee48b46309539e7373b0016
JoseBastidas-Prog <85695084+JoseBastidas-Prog@users.noreply.github.com>
2022-05-26 20:04:41 -0400

    created shared component
    Shared component 'package-item' added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -85,6 +85,7 @@ import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
 import { CartButtonComponent } from './components/cart-button/cart-button.component';
 import { CollaborationsComponent } from './dialogs/collaborations/collaborations.component';
 import { InfoButtonComponent } from './components/info-button/info-button.component';
+import { PackageItemComponent } from './components/package-item/package-item.component';
 
 const imports = [
   CommonModule,
@@ -167,6 +168,7 @@ const declarations = [
   CartButtonComponent,
   CollaborationsComponent,
   InfoButtonComponent,
+  PackageItemComponent,
 ];
 
 @NgModule({
