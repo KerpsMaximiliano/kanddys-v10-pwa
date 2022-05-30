84c2d17e01d686e058ab67330ca6b101ffa456a2
Diego <diegodlopezg@gmail.com>
2022-05-30 17:54:33 -0400

    Reverted lockUi changes. Need more testing

--- src/app/core/helpers/ui.helpers.ts
+++ src/app/core/helpers/ui.helpers.ts
@@ -1,20 +1,21 @@
 const blocker = document.createElement('div');
 blocker.classList.add('ui-blocker');
 blocker.innerHTML = `<i class="fad fa-spinner-third fa-spin"></i>`;
-let counter = 0;
+// let counter = 0;
 
 export function lockUI(waitFor?: Promise<any>) {
   try {
-    counter++;
+    // counter++;
     document?.body?.appendChild(blocker);
     waitFor?.finally(() => unlockUI());
   } catch (e) {}
 }
 
 export function unlockUI() {
-  if(!counter) return;
+  // if(!counter) return;
   try {
-    counter--;
-    if(counter === 0) document?.body?.removeChild(blocker);
+    // counter--;
+    // if(counter === 0) document?.body?.removeChild(blocker);
+    document?.body?.removeChild(blocker);
   } catch (e) {}
 }
