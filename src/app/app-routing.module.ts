4e75ee7aeb20c333c9094cef025279571be80652
Diego <diegodlopezg@gmail.com>
2022-06-06 19:20:54 -0400

    Deleted unused shared components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,24 @@
-import { PartnerReceiptsComponent } from './shared/pages/partner-receipts/partner-receipts.component';
-import { InvitationComponent } from './shared/pages/invitation/invitation.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './shared/pages/home/home.component';
 
 const redirectTo = '/home';
 const routes: Routes = [
   { path: '', redirectTo, pathMatch: 'full' },
-  { path: 'invitation/community/:identifier', component: InvitationComponent },
-  {
-    path: 'partner/receipts/:id',
-    component: PartnerReceiptsComponent,
-    data: { fullscreen: true },
-  },
-  {
-    path: 'home',
-    /*canLoad: [AuthGuard],
-    canActivate: [AuthGuard],
-    canActivateChild: [AuthGuard],*/
-    component: HomeComponent
-    //component: LandingComponent
-  },
+  // { path: 'invitation/community/:identifier', component: InvitationComponent },
+  // {
+  //   path: 'partner/receipts/:id',
+  //   component: PartnerReceiptsComponent,
+  //   data: { fullscreen: true },
+  // },
+  // {
+  //   path: 'home',
+  // canLoad: [AuthGuard],
+  // canActivate: [AuthGuard],
+  // canActivateChild: [AuthGuard],
+  // component: HomeComponent
+  // component: LandingComponent
+  // },
   {
     path: 'ecommerce',
     /*canLoad: [AuthGuard],
