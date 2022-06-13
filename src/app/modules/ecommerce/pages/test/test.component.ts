463395f39e633ba580996d94b66ed0e5489e1e81
Diego <diegodlopezg@gmail.com>
2022-06-13 14:19:32 -0400

    Made store-share component more dynamic

--- src/app/modules/ecommerce/pages/test/test.component.ts
+++ src/app/modules/ecommerce/pages/test/test.component.ts
@@ -3,6 +3,8 @@ import { DialogService } from 'src/app/libs/dialog/services/dialog.service';
 import { CustomFieldsComponent } from 'src/app/shared/dialogs/custom-fields/custom-fields.component';
 import { MagicLinkDialogComponent } from 'src/app/shared/components/magic-link-dialog/magic-link-dialog.component';
 import { CollaborationsComponent } from 'src/app/shared/dialogs/collaborations/collaborations.component';
+import { StoreShareComponent } from 'src/app/shared/dialogs/store-share/store-share.component';
+import { StoreShareOption } from 'src/app/shared/dialogs/store-share/store-share.component';
 
 @Component({
   selector: 'app-test',
@@ -26,11 +28,63 @@ export class TestComponent implements OnInit {
     //   customClass: 'app-dialog',
     //   flags: ['no-header'],
     // });
-
     this.dialog.open(CollaborationsComponent, {
       type: 'flat-action-sheet',
       customClass: 'app-dialog',
       flags: ['no-header'],
     });
+    // +++++++ Para verificar la task 958
+    const options: StoreShareOption[] = [
+      {
+        text: 'asd test',
+        mode: 'clipboard',
+        link: 'dasdasdasdsa',
+        icon: {
+          src: '/upload.svg',
+          size: {
+            width: 20,
+            height: 26
+          }
+        }
+      },
+      {
+        text: 'now sharing!',
+        mode: 'share',
+        link: 'this is the linkj to share',
+        icon: {
+          src: '/upload.svg',
+          size: {
+            width: 20,
+            height: 26
+          }
+        }
+      },
+      {
+        text: 'qr thing',
+        mode: 'qr',
+        link: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        icon: {
+          src: '/qr-code.svg',
+          size: {
+            width: 30,
+            height: 30
+          }
+        }
+      },
+      {
+        text: 'xd',
+        func: () => console.log('uwu'),
+      }
+    ];
+    this.dialog.open(StoreShareComponent, {
+      type: 'fullscreen-translucent',
+      props: {
+        title: 'test!!!',
+        options
+      },
+      customClass: 'app-dialog',
+      flags: ['no-header'],
+    });
+    // ------- Para verificar la task 958
   }
 }
