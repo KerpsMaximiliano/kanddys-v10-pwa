1c3daf5140643076702f52dec43acedf8308cf65
Luis Barboza <lbarbozanav@gmail.com>
2022-06-15 16:44:25 -0400

    update bubble buttons in multistep

--- src/app/modules/ecommerce/pages/item-creator/item-creator.component.ts
+++ src/app/modules/ecommerce/pages/item-creator/item-creator.component.ts
@@ -42,6 +42,7 @@ export class ItemCreatorComponent implements OnInit {
   footerConfig: FooterOptions = {
     bubbleConfig: {
       validStep: {
+        dontShow: true,
         right: { text: 'VISTA' },
         function: async (params) => {
           const values = params.dataModel.value;
@@ -383,7 +384,8 @@ export class ItemCreatorComponent implements OnInit {
                   purchaseLocations: [],
                 });
 
-                if ('_id' in createPreItem) this.router.navigate([`/ecommerce/item-display/${createPreItem?._id}`]);
+                // if ('_id' in createPreItem) this.router.navigate([`/ecommerce/item-display/${createPreItem?._id}`]);
+                if ('_id' in createPreItem) this.router.navigate([`/ecommerce/new-item-contact-info/${createPreItem?._id}`]);
               }
             }
           } catch (error) {
