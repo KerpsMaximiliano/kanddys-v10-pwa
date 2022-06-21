1501182b77106c34872b6db9972d1870fc86c677
JoseBastidas-Prog <85695084+JoseBastidas-Prog@users.noreply.github.com>
2022-06-21 17:36:43 -0400

    updated
    updated sticy button component. Added mode 'double'

--- src/app/shared/components/sticky-button/sticky-button.component.ts
+++ src/app/shared/components/sticky-button/sticky-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
 import { NgNavigatorShareService } from 'ng-navigator-share';
 import { copyText } from 'src/app/core/helpers/strings.helpers';
 import { notification } from 'onsenui';
@@ -11,6 +11,7 @@ import { notification } from 'onsenui';
 export class StickyButtonComponent implements OnInit {
   @Input() mode: string = 'basic';
   @Input() text: string;
+  @Input() text2: string;
   @Input() auxText: string;
   @Input() link: string = 'linktest.com';
   @Input() icon: string;
@@ -19,6 +20,9 @@ export class StickyButtonComponent implements OnInit {
   @Input() height: string = null;  
   @Input() fontSize: string = null;  
   @Input() size: 'small' | 'normal' = "normal";
+
+  @Output() left = new EventEmitter;
+  @Output() right = new EventEmitter;
 
   private ngNavigatorShareService: NgNavigatorShareService;
 
@@ -87,4 +91,13 @@ export class StickyButtonComponent implements OnInit {
         console.log(error);
       });
   }
+  
+  leftButton(){
+    this.left.emit();
+  }
+
+  rightButton(){
+    this.right.emit();
+  }
+
 }
