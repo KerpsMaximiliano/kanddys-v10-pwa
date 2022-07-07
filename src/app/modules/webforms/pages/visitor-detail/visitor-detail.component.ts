f9302d2481b45629e72f42e4fb8a39c94e1f469a
JoseBastidas-Prog <85695084+JoseBastidas-Prog@users.noreply.github.com>
2022-07-07 13:23:53 -0400

    helper-header

--- src/app/modules/webforms/pages/visitor-detail/visitor-detail.component.ts
+++ src/app/modules/webforms/pages/visitor-detail/visitor-detail.component.ts
@@ -8,20 +8,6 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./visitor-detail.component.scss']
 })
 export class VisitorDetailComponent implements OnInit {
-
-    answerList: [{
-        headline: 'Respuesta ID',
-        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sapiente impedit in, soluta nam aperiam facere. Rem est sunt dolorum ipsum ratione, quam distinctio reiciendis inventore, aliquid natus perferendis laudantium!',
-        date: '06/06/2022'
-    },{
-        headline: 'Respuesta ID',
-        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sapiente impedit in, soluta nam aperiam facere. Rem est sunt dolorum ipsum ratione, quam distinctio reiciendis inventore, aliquid natus perferendis laudantium!',
-        date: '06/06/2022'
-    },{
-        headline: 'Respuesta ID',
-        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sapiente impedit in, soluta nam aperiam facere. Rem est sunt dolorum ipsum ratione, quam distinctio reiciendis inventore, aliquid natus perferendis laudantium!',
-        date: '06/06/2022'
-    },];
     env: string = environment.assetsUrl;
 
   constructor(
