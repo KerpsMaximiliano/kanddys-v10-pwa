9071a7a165cbc9f47cef3c2b36183ee4d840d417
Luis Barboza <lbarbozanav@gmail.com>
2022-07-20 15:57:28 -0400

    saving changes x2

--- src/app/shared/components/question-item/question-item.component.ts
+++ src/app/shared/components/question-item/question-item.component.ts
@@ -12,6 +12,12 @@ interface QuestionForm{
 
 interface FakeTag extends Tag {
   selected?: boolean;
+  icon?: {
+    src: string,
+    callback?(params): any;
+    width: number;
+    height: number;
+  };
 }
 
 @Component({
@@ -24,12 +30,38 @@ export class QuestionItemComponent implements OnInit {
   @Input() single: boolean;
   @Input() icon: string;
   @Input() headline: string;
+  @Input() headlineLeftIcon: {
+    icon: string,
+    callback?(params): any;
+    width: number;
+    height: number;
+  } = null;
+  @Input() headlineRightIcon: {
+    icon: string,
+    callback?(params): any;
+    width: number;
+    height: number;
+  } = null;
+  @Input() headlineRightText: {
+    text: string,
+    callback?(...params): any;
+    color: string;
+    fontSize: string;
+    fontFamily: string;
+  } = null;
+  @Input() headlineTitleStyles: Record<string, any> = null;
+  @Input() headlineRightTextStyles: Record<string, any> = null;
+  @Input() headlineContainerStyles: Record<string, any> = null;
+  @Input() tagListStyles: Record<string, any> = null;
+  @Input() tagListTextStyles: Record<string, any> = null;
   @Input() answer: string;
   @Input() date: string;
   @Input() total: number;
   @Input() data: QuestionForm[];
   @Input() tags?: FakeTag[];
+  @Input() collapseTags?: boolean = true;
   @Input() tagsLabel: string;
+  @Input() chipMode: number = 1;
   @Input() user?: User;
   @Input() users?: User[];
   showTags: boolean;
