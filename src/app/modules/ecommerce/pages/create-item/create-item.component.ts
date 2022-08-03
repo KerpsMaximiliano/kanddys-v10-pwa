8307d27a3e4492fb0251986ff26967c1d9dcaaa7
Diego <diegodlopezg@gmail.com>
2022-08-03 15:52:12 -0400

    New create item WIP

--- src/app/modules/ecommerce/pages/create-item/create-item.component.ts
+++ src/app/modules/ecommerce/pages/create-item/create-item.component.ts
@@ -5,19 +5,22 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-create-item',
   templateUrl: './create-item.component.html',
-  styleUrls: ['./create-item.component.scss']
+  styleUrls: ['./create-item.component.scss'],
 })
 export class CreateItemComponent implements OnInit {
   env = environment.assetsUrl;
+  defaultImages: (string | ArrayBuffer)[] = [''];
+  imagesAlreadyLoaded: boolean;
   item: Item;
+  mode: 'main' | 'name' = 'main';
+  disableFooter = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   goBack() {
-    //
+    if (this.mode === 'name') return (this.mode = 'main');
   }
 
   toggleStatus() {
