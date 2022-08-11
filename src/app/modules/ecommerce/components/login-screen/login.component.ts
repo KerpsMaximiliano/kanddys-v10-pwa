770217e76849628c5010f921f4d091faabf76938
JoseBastidas-Prog <85695084+JoseBastidas-Prog@users.noreply.github.com>
2022-08-11 18:54:34 -0400

    login wip

--- src/app/modules/ecommerce/components/login-screen/login.component.ts
+++ src/app/modules/ecommerce/components/login-screen/login.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 import {
   SearchCountryField,
   CountryISO,
   PhoneNumberFormat,
 } from 'ngx-intl-tel-input';
+import { lockUI, unlockUI } from 'src/app/core/helpers/ui.helpers';
+import { notification } from 'onsenui';
 
 @Component({
   selector: 'app-login',
@@ -13,9 +17,11 @@ import {
 })
 export class LoginComponent implements OnInit {
 
+    auth: 'phone' | 'password';
     merchantNumber: string = '(000) 000-0000';
+    loggin: boolean;
     phoneNumber = new FormControl('', [Validators.minLength(10)]);
-    loggin: boolean = true;
+    password = new FormControl('', [Validators.required, Validators.minLength(3)] )
     SearchCountryField = SearchCountryField;
     CountryISO = CountryISO.DominicanRepublic;
     preferredCountries: CountryISO[] = [
@@ -25,12 +31,64 @@ export class LoginComponent implements OnInit {
     PhoneNumberFormat = PhoneNumberFormat;
 
 
-  constructor() { }
+  constructor(     
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private router: Router
+    ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const phone = this.route.snapshot.queryParamMap.get('phone');
+    this.auth = this.route.snapshot.queryParamMap.get('auth') as 'phone' | 'password';
+
+    if(this.auth === 'password') {
+        lockUI();
+
+        if(phone) {
+            try {
+                const {countryIso, nationalNumber} = await this.authService.getPhoneInformation(phone);
+                this.phoneNumber.setValue(nationalNumber);
+                this.CountryISO = countryIso;
+                this.loggin = true;
+                this.merchantNumber = phone;
+                
+            } catch(e){
+                unlockUI();
+                notification.toast('Número no registrado o inválido', {timeout: 2000});
+            }
+
+            unlockUI();
+        } else {
+            this.loggin = false;
+            unlockUI();
+        }
+
+    } else {
+        this.auth = 'phone';
+        this.loggin = false;
+        unlockUI(); 
+    }
+
   }
 
   toggleLog(){
     this.loggin = !this.loggin;
   }
+
+  async submitPhone(){
+    console.log(this.phoneNumber);
+    
+    try{
+        const {countryIso, nationalNumber} = await this.authService.getPhoneInformation(this.phoneNumber.value.e164Number);
+        this.merchantNumber = this.phoneNumber.value.e164Number.split('+')[1];
+        this.phoneNumber.setValue(nationalNumber);
+        this.CountryISO = countryIso;
+
+    } catch (error) {
+        notification.toast('Número no registrado o Inválido', {timeout: 2000});
+        console.log(error);
+    }
+
+  };
+
 }
