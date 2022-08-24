81b4041d62dbc6a1ed884fb69396e0505343403e
Diego <diegodlopezg@gmail.com>
2022-08-24 12:57:49 -0400

    Store redesign

--- src/app/shared/components/item/item.component.ts
+++ src/app/shared/components/item/item.component.ts
@@ -1,17 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Item } from 'src/app/core/models/item';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.scss']
+  styleUrls: ['./item.component.scss'],
 })
 export class ItemComponent implements OnInit {
   @Input() item: Item;
+  @Output() imageEvent = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
