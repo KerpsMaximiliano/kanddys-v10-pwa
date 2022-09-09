807db9a8b5d8cfc8579d5283e2c35c488a27df71
Luis Barboza <lbarbozanav@gmail.com>
2022-09-09 14:33:33 -0400

    fixed imageinput style in create item

--- src/app/shared/components/currency-input/currency-input.component.ts
+++ src/app/shared/components/currency-input/currency-input.component.ts
@@ -10,6 +10,7 @@ export class CurrencyInputComponent implements OnInit {
   curencyFocused = false;
   formattedPricing = '$0.00';
   @Input() initialValue: number;
+  @Input() fieldStyles: Record<string, any> = null;
   @Output() onInputEvent = new EventEmitter<number>();
 
   constructor(private decimalPipe: DecimalPipe) {}
