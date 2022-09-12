188035cb5e90a2b7b7e1ae8746980ac8ad9e0d0b
Jorge Chourio <jorge_chourio@hotmail.com>
2022-09-12 17:53:00 -0400

    Fixed bug showing images of dynamic items in checkout and order-detail components

--- src/app/modules/ecommerce/pages/checkout/checkout.component.ts
+++ src/app/modules/ecommerce/pages/checkout/checkout.component.ts
@@ -32,7 +32,7 @@ export class CheckoutComponent implements OnInit {
   };
   saleflow: SaleFlow;
   order: ItemOrderInput;
-  items: Item[];
+  items: any[];
   post: PostInput;
   payment: number;
   hasPayment: boolean;
@@ -150,6 +150,7 @@ export class CheckoutComponent implements OnInit {
       this.headerService.saleflow || this.headerService.getSaleflow();
     this.order = this.headerService.getOrder(this.saleflow?._id);
     this.items = this.headerService.getItems(this.saleflow?._id);
+    console.log(this.items);
     this.post =
       this.headerService.post ||
       this.headerService.getPost(
