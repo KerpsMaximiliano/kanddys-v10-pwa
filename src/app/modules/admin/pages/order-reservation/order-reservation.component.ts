ebf79403d592747f0360a34ac54d1d34563eba75
JoseBastidas-Prog <85695084+JoseBastidas-Prog@users.noreply.github.com>
2022-09-15 14:43:23 -0400

    order reservation WIP

--- src/app/modules/admin/pages/order-reservation/order-reservation.component.ts
+++ src/app/modules/admin/pages/order-reservation/order-reservation.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { OptionAnswerSelector } from 'src/app/core/types/answer-selector';
+import { DialogService } from 'src/app/libs/dialog/services/dialog.service';
+import {
+  StoreShareComponent,
+  StoreShareList,
+} from 'src/app/shared/dialogs/store-share/store-share.component';
 
 @Component({
   selector: 'app-order-reservation',
   templateUrl: './order-reservation.component.html',
-  styleUrls: ['./order-reservation.component.scss']
+  styleUrls: ['./order-reservation.component.scss'],
 })
 export class OrderReservationComponent implements OnInit {
+  constructor(private dialogService: DialogService) {}
 
-  constructor() { }
-
+  mode: 'default' | 'edit' | 'delete';
   optionIndex: number;
+  optionIndexArray: number[] = [];
   dummyOptions: OptionAnswerSelector[] = [
-   {
+    {
       status: true,
       id: 'date1',
       value: 'Date to Date ID',
@@ -30,21 +36,21 @@ export class OrderReservationComponent implements OnInit {
           },
         },
         {
-         text: 'Factura ID',
+          text: 'Factura ID',
           styles: {
             fontFamily: 'SfProRegular',
             fontSize: '1.063rem',
           },
         },
         {
-         text: 'RD $ 1,450.00 ',
-         styles:{
+          text: 'RD $ 1,450.00 ',
+          styles: {
             fontFamily: 'SfProBold',
             fontSize: '1.063rem',
             color: '#174B72',
-            margin: ' 10px 32px'
-         }
-        }
+            margin: ' 10px 32px',
+          },
+        },
       ],
     },
     {
@@ -65,21 +71,21 @@ export class OrderReservationComponent implements OnInit {
           },
         },
         {
-         text: 'Factura ID',
+          text: 'Factura ID',
           styles: {
             fontFamily: 'SfProRegular',
             fontSize: '1.063rem',
           },
         },
         {
-         text: 'RD $ 1,450.00 ',
-         styles:{
+          text: 'RD $ 1,450.00 ',
+          styles: {
             fontFamily: 'SfProBold',
             fontSize: '1.063rem',
             color: '#174B72',
-            margin: ' 10px 32px'
-         }
-        }
+            margin: ' 10px 32px',
+          },
+        },
       ],
     },
     {
@@ -100,21 +106,21 @@ export class OrderReservationComponent implements OnInit {
           },
         },
         {
-         text: 'Factura ID',
+          text: 'Factura ID',
           styles: {
             fontFamily: 'SfProRegular',
             fontSize: '1.063rem',
           },
         },
         {
-         text: 'RD $ 1,450.00 ',
-         styles:{
+          text: 'RD $ 1,450.00 ',
+          styles: {
             fontFamily: 'SfProBold',
             fontSize: '1.063rem',
             color: '#174B72',
-            margin: ' 10px 32px'
-         }
-        }
+            margin: ' 10px 32px',
+          },
+        },
       ],
     },
     {
@@ -135,21 +141,21 @@ export class OrderReservationComponent implements OnInit {
           },
         },
         {
-         text: 'Factura ID',
+          text: 'Factura ID',
           styles: {
             fontFamily: 'SfProRegular',
             fontSize: '1.063rem',
           },
         },
         {
-         text: 'RD $ 1,450.00 ',
-         styles:{
+          text: 'RD $ 1,450.00 ',
+          styles: {
             fontFamily: 'SfProBold',
             fontSize: '1.063rem',
             color: '#174B72',
-            margin: ' 10px 32px'
-         }
-        }
+            margin: ' 10px 32px',
+          },
+        },
       ],
     },
     {
@@ -170,28 +176,92 @@ export class OrderReservationComponent implements OnInit {
           },
         },
         {
-         text: 'Factura ID',
+          text: 'Factura ID',
           styles: {
             fontFamily: 'SfProRegular',
             fontSize: '1.063rem',
           },
         },
         {
-         text: 'RD $ 1,450.00 ',
-         styles:{
+          text: 'RD $ 1,450.00 ',
+          styles: {
             fontFamily: 'SfProBold',
             fontSize: '1.063rem',
             color: '#174B72',
-            margin: ' 10px 32px'
-         }
-        }
+            margin: ' 10px 32px',
+          },
+        },
       ],
     },
-  ]; 
-  ngOnInit(): void {
+  ];
+  ngOnInit(): void {}
+
+  optionClicked(e) {
+    console.log(e);
   }
 
-  optionClicked(e){
-   console.log(e)
+  openDialog() {
+    const list: StoreShareList[] = [
+      {
+        title: 'RESERVACIONES',
+        options: [
+          {
+            text: 'Crear reservación a un cliente'.toUpperCase(),
+            mode: 'func',
+            func: () => {
+              console.log('N/A');
+            },
+          },
+          {
+            text: 'EDITAR',
+            mode: 'func',
+            func: () => {
+              this.mode === 'edit';
+              console.log(this.mode)
+            },
+          },
+          {
+            text: 'BORRAR',
+            mode: 'func',
+            func: () => {
+              this.mode == 'delete';
+              console.log(this.mode)
+            },
+          },
+          {
+            text: 'BLOQUEA SLOTS',
+            mode: 'func',
+            func: () => {
+              console.log('N/A');
+            },
+          },
+          {
+            text: 'COMPARTE EL LINK DE SLOTS ',
+            mode: 'func',
+            func: () => {
+              console.log('N/A');
+            },
+          },
+        ],
+      },
+    ];
+
+    this.dialogService.open(StoreShareComponent, {
+      type: 'fullscreen-translucent',
+      props: {
+        list,
+        hideCancelButtton: true,
+        alternate: true,
+        dynamicStyles: {
+          titleWrapper: {
+            display: 'flex',
+            alignItems: 'flex-start',
+            paddingBottom: '26px',
+          },
+        },
+      },
+      customClass: 'app-dialog',
+      flags: ['no-header'],
+    });
   }
 }
