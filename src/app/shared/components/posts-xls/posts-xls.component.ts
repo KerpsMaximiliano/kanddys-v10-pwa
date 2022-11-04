6948424a952f1b2473f7296edb0bcaacd1892fa0
glbrtjose <gilbertocarrizo@gmail.com>
2022-11-04 17:13:22 -0400

    p3.6-features: Hacer lógica e incluir funcionalidad para exportar en un .xls

--- src/app/shared/components/posts-xls/posts-xls.component.ts
+++ src/app/shared/components/posts-xls/posts-xls.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ExportExcelService } from 'src/app/core/services/export-excel.service';
 import { PostsService } from 'src/app/core/services/posts.service';
 
 @Component({
@@ -12,19 +13,43 @@ export class PostsXlsComponent implements OnInit {
     Validators.required,
     Validators.pattern('^[0-9]*$'),
   ]);
-  constructor(private _PostsService: PostsService) {}
+  dataForExcel = [];
+  empPerformance = [];
+  constructor(
+    private _PostsService: PostsService,
+    public ete: ExportExcelService
+  ) {}
 
   ngOnInit(): void {}
 
   submit(): void {
     if (this.controller.invalid) return;
-    console.log(this.controller.value);
-    for (const item of this.fillList(+this.controller.value))
-      this._PostsService.createPost({});
+    let result = [];
+    const createPosts = async () => {
+      for (const item of this.fillList(+this.controller.value))
+        result.push(await this._PostsService.createPost({}));
+      result = result.map(({ createPost }) => createPost);
+      this.exportToExcel(result);
+    };
+    createPosts();
   }
 
   fillList(n: number): any[] {
     const result = [...Array(n).keys()].map(() => '');
     return result;
+  }
+
+  exportToExcel(list) {
+    list.forEach((row: any) => {
+      this.dataForExcel.push(Object.values(row));
+    });
+
+    let reportData = {
+      title: 'Post Report',
+      data: this.dataForExcel,
+      headers: Object.keys(list[0]),
+    };
+
+    this.ete.exportExcel(reportData);
   }
 }
