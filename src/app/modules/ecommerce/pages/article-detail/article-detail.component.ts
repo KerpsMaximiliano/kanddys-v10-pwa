fd3a2fc5f276ec488e0853ea68e7865a6cae40f9
Diego <diegodlopezg@gmail.com>
2022-11-15 18:55:30 -0400

    Bugfixes

--- src/app/modules/ecommerce/pages/article-detail/article-detail.component.ts
+++ src/app/modules/ecommerce/pages/article-detail/article-detail.component.ts
@@ -38,6 +38,10 @@ export class ArticleDetailComponent implements OnInit {
     slidesPerView: 1,
     freeMode: false,
     spaceBetween: 0,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
     autoplay: {
       delay: 10000,
       stopOnLastSlide: true,
@@ -401,9 +405,10 @@ export class ArticleDetailComponent implements OnInit {
       .map(
         () =>
           `${
-            this.itemData.images.length < 3
-              ? '1'
-              : this.getRandomArbitrary(0, this.itemData.images.length)
+            // this.itemData.images.length < 3
+            //   ?
+            '1'
+            // : this.getRandomArbitrary(0, this.itemData.images.length)
           }fr`
       )
       .join(' ');
