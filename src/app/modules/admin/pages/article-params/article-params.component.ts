5ccdc79a418cd31e3a6a8345365e41ff9ad843cf
Jorge Chourio <jorge_chourio@hotmail.com>
2022-12-06 14:23:47 -0400

    Changed UX flow in article-params to create/update an item

--- src/app/modules/admin/pages/article-params/article-params.component.ts
+++ src/app/modules/admin/pages/article-params/article-params.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { lockUI, unlockUI } from 'src/app/core/helpers/ui.helpers';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router, ActivatedRoute } from '@angular/router';
 import { SaleFlow } from 'src/app/core/models/saleflow';
@@ -42,6 +43,7 @@ export class ArticleParamsComponent implements OnInit {
     Validators.minLength(2),
     Validators.pattern(/[\S]/),
   ]);
+  updated: boolean = false;
   blockSubmitButton: boolean = false;
   parseFloat = parseFloat;
 
@@ -107,12 +109,70 @@ export class ArticleParamsComponent implements OnInit {
     });
   }
 
-  iconCallback = () => {
-    this._ItemsService.itemName = this.name.value;
-    this._ItemsService.itemPrice = this.price.value;
-    this._Router.navigate([
-      `admin/create-article${this.item ? '/' + this.item._id : ''}`,
-    ]);
+  iconCallback = async () => {
+    // this._ItemsService.itemName = this.name.value;
+    // this._ItemsService.itemPrice = this.price.value;
+
+    const itemInput = {
+      name: this.name.value || null,
+      // description: description || null,
+      pricing: this.price.value,
+      images: this._ItemsService.itemImages,
+      merchant: this._MerchantsService.merchantData?._id,
+      content: [],
+      currencies: [],
+      hasExtraPrice: false,
+      purchaseLocations: [],
+      showImages: this._ItemsService.itemImages.length > 0,
+    };
+    this._ItemsService.itemPrice = null;
+    this._ItemsService.itemName = null;
+
+    if (this.item) {
+      if (this.updated || this._ItemsService.changedImages) {
+        lockUI();
+        delete itemInput.images;
+        delete itemInput.merchant;
+        const { updateItem: updatedItem } = await this._ItemsService.updateItem(
+          itemInput,
+          this.item._id
+        );
+        if (this._ItemsService.changedImages) {
+          await this._ItemsService.deleteImageItem(
+            this.item.images,
+            updatedItem._id
+          );
+          await this._ItemsService.addImageItem(
+            this._ItemsService.itemImages,
+            updatedItem._id
+          );
+          this._ItemsService.itemImages = [];
+          this._ItemsService.changedImages = false;
+        }
+      }
+      unlockUI();
+      this._ItemsService.removeTemporalItem();
+      this._Router.navigate([
+        `admin/create-article${this.item ? '/' + this.item._id : ''}`,
+      ]);
+    } else {
+      if (this.price.value) {
+        lockUI();
+        console.log(this.price.value);
+        const { createItem } = await this._ItemsService.createItem(itemInput);
+        await this._SaleflowService.addItemToSaleFlow(
+          {
+            item: createItem._id,
+          },
+          this._SaleflowService.saleflowData._id
+        );
+        unlockUI();
+        this._ToastrService.success('Producto creado satisfactoriamente!');
+        this._Router.navigate([`/admin/create-article/${createItem._id}`]);
+      } else {
+        this._Router.navigate([`/admin/create-article`]);
+      }
+    }
   };
 
   dotsCallback = () => {
@@ -342,7 +402,12 @@ export class ArticleParamsComponent implements OnInit {
   }
 
   handleCurrencyInput(value: number) {
+    this.updated = true;
     this.price.setValue(value);
+  }
+
+  handleNameInput(value: any) {
+    this.updated = true;
   }
 
   openImageModal(imageSourceURL: string | ArrayBuffer) {
