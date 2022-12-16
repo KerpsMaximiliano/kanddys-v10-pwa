7dd848069c26b59e4851f0c8f3fb9af31cbca0f2
glbrtjose <gilbertocarrizo@gmail.com>
2022-12-16 18:55:58 -0400

    redirections portions of code updated

--- src/app/modules/ecommerce/pages/article-access/article-access.component.ts
+++ src/app/modules/ecommerce/pages/article-access/article-access.component.ts
@@ -189,11 +189,10 @@ export class ArticleAccessComponent implements OnInit, OnDestroy {
    this.sentInvite = true;
    this.code = this.check[e].subtexts.length?this.check[e].subtexts[0].text:this.check[e].value; 
    const generateMagicLink = async () => {
-      const { entity, reference } = await this._EntityTemplateService.entityTemplateRecipient(this.templateId);
       const emailOrPhone = this.targets[e].phone || this.targets[e].email;
       const result = await this._AuthService.generateMagicLink(
          emailOrPhone.replace('+',''),
-         `qr/article-detail/${entity}/${reference}`,
+         'qr/article-detail',
          this.templateId,
          'TemplateAccess',
          {}
