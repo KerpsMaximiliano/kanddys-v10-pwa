92bf3d40cdadc195f2ffb3e6a3dbae8a6e17acc3
Luis Barboza <lbarbozanav@gmail.com>
2022-12-21 18:24:08 -0400

    task 2460 dialog flow component

--- src/app/shared/components/dialog-flow/dialog-flow.component.ts
+++ src/app/shared/components/dialog-flow/dialog-flow.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, ViewChild, EventEmitter } from '@angular/core';
 import { EmbeddedComponent } from 'src/app/core/types/multistep-form';
-import { SwiperOptions } from 'swiper';
+import { SwiperOptions, Swiper } from 'swiper';
+import { SwiperComponent } from 'ngx-swiper-wrapper';
 import { BlankComponent } from '../../dialogs/blank/blank.component';
 
 @Component({
@@ -9,53 +10,48 @@ import { BlankComponent } from '../../dialogs/blank/blank.component';
   styleUrls: ['./dialog-flow.component.scss'],
 })
 export class DialogFlowComponent implements OnInit {
-  @Input() dialogs: Array<EmbeddedComponent> = [
-    {
-      component: BlankComponent,
-      inputs: {
-        containerStyles: {
-          height: '500px',
-          width: '90%',
-        },
-      },
-    },
-    {
-      component: BlankComponent,
-      inputs: {
-        containerStyles: {
-          height: '200px',
-          width: '90%',
-        },
-      },
-    },
-    {
-      component: BlankComponent,
-      inputs: {
-        containerStyles: {
-          height: '200px',
-          width: '90%',
-        },
-      },
-    },
-    {
-      component: BlankComponent,
-      inputs: {
-        containerStyles: {
-          height: '200px',
-          width: '90%',
-        },
-      },
-    },
-  ];
+  @Input() dialogs: Array<EmbeddedComponent> = [];
+  @Output() saveConfigRef = new EventEmitter();
   swiperConfig: SwiperOptions = {
     direction: 'vertical',
     centeredSlides: true,
     slidesPerView: 'auto',
-    spaceBetween: 30
+    spaceBetween: 17,
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
   };
   @Input() status: 'OPEN' | 'CLOSE' = 'CLOSE';
+  currentDialogIndex: number = 0;
+
+  @ViewChild('dialogSwiper') dialogSwiper: SwiperComponent;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.applyTransparencyToSlidesThatArentActive();
+      this.swiperConfig.allowSlideNext = false;
+      this.saveConfigRef.emit(this.swiperConfig);
+    }, 100);
+  }
+
+  applyTransparencyToSlidesThatArentActive() {
+    const allSlides = document.querySelectorAll('.swiper-slide');
+
+    allSlides.forEach((slide, index) => {
+      const dialogHTMLElement = slide;
+
+      if (index !== this.currentDialogIndex) {
+        this.dialogs[index].inputs.containerStyles.opacity = '0.5';
+      } else {
+        this.dialogs[index].inputs.containerStyles.opacity = '1';
+      }
+    });
+  }
+
+  changeActiveDialog(eventData: Swiper) {
+    this.currentDialogIndex = eventData.activeIndex;
+
+    this.applyTransparencyToSlidesThatArentActive();
+  }
 }
