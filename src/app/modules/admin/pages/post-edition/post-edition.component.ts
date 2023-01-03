3aa048245780106f23b7c2feb46d24b5757a6477
GabrielSR07 <GabrielSR07@users.noreply.github.com>
2023-01-03 15:54:15 -0400

    .

--- src/app/modules/admin/pages/post-edition/post-edition.component.ts
+++ src/app/modules/admin/pages/post-edition/post-edition.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-edition',
   templateUrl: './post-edition.component.html',
-  styleUrls: ['./post-edition.component.scss']
+  styleUrls: ['./post-edition.component.scss'],
 })
 export class PostEditionComponent implements OnInit {
+  @Input() nombreSobre: string = 'La mujer que le dicen DaVest!!';
+  @Input() mensaje: string =
+    'Por conseguir lo que veias imposible, por creer en ti, y por demostrarnos a todos lo grande y capaz que eres.';
+  4;
+  @Input() from: string = 'James Bond';
+  default: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
