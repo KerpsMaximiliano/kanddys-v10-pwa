c311c5e04dad76678761bc03fcca8c3dd6441930
glbrtjose <gilbertocarrizo@gmail.com>
2023-01-05 15:07:06 -0400

    2524: Integrar flujo de pantallas para la edición de la información de contacto del merchant

--- src/app/core/models/contact.ts
+++ src/app/core/models/contact.ts
@@ -1,3 +1,5 @@
+import { Model } from "../objects/model";
+
 export class ContactInput {
   description?: string;
   image?: any;
@@ -5,4 +7,23 @@ export class ContactInput {
   name?: String;
   decription?: String;
   banner?: string;
+}
+
+export class Link extends Model<Link> {
+  _id: string;
+  image: String;
+  name: String;
+  value: String;
+}
+
+export class Contact extends Model<Contact> {
+  _id: string;
+  description: string;
+  image: string;
+  merchant: string;
+  name: string;
+  decription: string;
+  link: Link[];
+  banner: string;
+  default: boolean;
 }
--- src/app/core/services/contact.service.ts
+++ src/app/core/services/contact.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { contactAddLink, createContact } from '../graphql/contact.gql';
+import { contactAddLink, contacts, contactUpdateLink, createContact, updateContact } from '../graphql/contact.gql';
 import { GraphQLWrapper } from '../graphql/graphql-wrapper.service';
-import { ContactInput } from '../models/contact';
+import { Contact, ContactInput } from '../models/contact';
 import { LinkInput } from '../models/LinkInput';
+import { PaginationInput } from '../models/saleflow';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,6 @@ export class ContactService {
   constructor(private graphql: GraphQLWrapper) {}
 
   async createContact(input: ContactInput) {
-
     const result = await this.graphql.mutate({
       mutation: createContact,
       variables: { input },
@@ -26,8 +26,21 @@ export class ContactService {
     return result.createContact;
   }
 
-  async contactAddLink(input: LinkInput, id: string) {
+  async updateContact(id:string, input: ContactInput) {
+    const result = await this.graphql.mutate({
+      mutation: updateContact,
+      variables: { id, input },
+      context: {
+        useMultipart: true,
+      },
+    });
 
+    if (!result || result?.errors) return undefined;
+
+    return result.createContact;
+  }
+
+  async contactAddLink(input: LinkInput, id: string) {
     const result = await this.graphql.mutate({
       mutation: contactAddLink,
       variables: { input, id },
@@ -39,5 +52,35 @@ export class ContactService {
     if (!result || result?.errors) return undefined;
 
     return result.contactAddLink;
+  }
+
+  async contactUpdateLink(input: LinkInput, linkId:string, id: string) {
+    const result = await this.graphql.mutate({
+      mutation: contactUpdateLink,
+      variables: { input, linkId, id },
+      context: {
+        useMultipart: true,
+      },
+    });
+
+    if (!result || result?.errors) return undefined;
+
+    return result.contactUpdateLink;
+  }
+
+  async contacts(
+    paginate: PaginationInput = { options: { limit: -1 } }
+  ): Promise<Contact[]> {
+    try {
+      const result = await this.graphql.query({
+        query: contacts,
+        variables: { paginate },
+        fetchPolicy: 'no-cache',
+      });
+      if (!result) return undefined;
+      return result.contacts;
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
