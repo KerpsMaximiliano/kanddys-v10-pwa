3b62c0ff3e57eb4340163ee2369fb73e0b221bdb
Diego <diegodlopezg@gmail.com>
2023-01-06 13:24:43 -0400

    Bugfix

--- src/app/modules/ecommerce/ecommerce/ecommerce.component.ts
+++ src/app/modules/ecommerce/ecommerce/ecommerce.component.ts
@@ -7,8 +7,8 @@ import { AppService } from 'src/app/app.service';
 import { HeaderService } from 'src/app/core/services/header.service';
 import { MerchantsService } from 'src/app/core/services/merchants.service';
 import { SaleFlowService } from 'src/app/core/services/saleflow.service';
-import { DialogService } from 'src/app/libs/dialog/services/dialog.service';
-import { ShowItemsComponent } from 'src/app/shared/dialogs/show-items/show-items.component';
+// import { DialogService } from 'src/app/libs/dialog/services/dialog.service';
+// import { ShowItemsComponent } from 'src/app/shared/dialogs/show-items/show-items.component';
 
 @Component({
   selector: 'app-ecommerce',
@@ -22,7 +22,7 @@ export class EcommerceComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private dialogService: DialogService,
+    // private dialogService: DialogService,
     public headerService: HeaderService,
     private appService: AppService,
     private _MerchantsService: MerchantsService,
