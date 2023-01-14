1c3b36e229c5aaf7529fad529c74064f6bb780a6
Jorge Chourio <jorge_chourio@hotmail.com>
2023-01-14 01:17:10 -0400

    Post's slides storing in postsservice

--- src/app/shared/components/qr-edit/qr-edit.component.ts
+++ src/app/shared/components/qr-edit/qr-edit.component.ts
@@ -5,6 +5,9 @@ import { PostInput, SlideInput } from 'src/app/core/models/post';
 import { PostsService } from 'src/app/core/services/posts.service';
 import { Router } from '@angular/router';
 import { HeaderService } from 'src/app/core/services/header.service';
+import { DialogService } from 'src/app/libs/dialog/services/dialog.service';
+import { SettingsComponent } from '../../dialogs/settings/settings.component';
+import { SingleActionDialogComponent } from '../../dialogs/single-action-dialog/single-action-dialog.component';
 
 @Component({
   selector: 'app-qr-edit',
@@ -23,7 +26,8 @@ export class QrEditComponent implements OnInit {
   constructor(
     private _PostsService: PostsService,
     private _Router: Router,
-    private headerService: HeaderService
+    private headerService: HeaderService,
+    private dialog: DialogService
   ) {}
 
   ngOnInit(): void {
@@ -31,6 +35,13 @@ export class QrEditComponent implements OnInit {
       const storedPost = localStorage.getItem('post');
 
       if (storedPost) this._PostsService.post = JSON.parse(storedPost);
+
+      console.log(this._PostsService.post);
+    }
+
+    this._PostsService.post = {
+      ...this._PostsService.post,
+      slides: this._PostsService.post?.slides ? this._PostsService.post?.slides : []
     }
 
     this.availableFiles = [
@@ -76,6 +87,7 @@ export class QrEditComponent implements OnInit {
       content['background'] = result;
       content['_type'] = file.type;
       this.gridArray.push(content);
+      this._PostsService.post.slides.push(content);
     };
   }
 
@@ -95,11 +107,57 @@ export class QrEditComponent implements OnInit {
 
     this._PostsService.post.slides = slides;
 
+    console.log(this._PostsService.post.slides);
+
     this._Router.navigate([
       'ecommerce',
       this.headerService.saleflow.merchant.slug,
       'post-edition',
     ]);
     return;
+  }
+
+  openSlideSettings(index: number) {
+    const list = [
+      {
+        text: 'Eliminar',
+        callback: async () => {
+          this.dialog.open(SingleActionDialogComponent, {
+            type: 'fullscreen-translucent',
+            props: {
+              title: 'Eliminar este slide del símbolo',
+              buttonText: 'Si, borrar',
+              mainButton: () => {
+                this.deleteImage(index);
+              },
+              btnBackgroundColor: '#272727',
+              btnMaxWidth: '133px',
+              btnPadding: '7px 2px',
+            },
+            customClass: 'app-dialog',
+            flags: ['no-header'],
+          });
+        },
+      }
+    ];
+    this.dialog.open(
+      SettingsComponent,
+      {
+        type: 'fullscreen-translucent',
+        props: {
+          optionsList: list,
+          closeEvent: () => {},
+          shareBtn: false,
+          title: '',
+        },
+        customClass: 'app-dialog',
+        flags: ['no-header'],
+      }
+    )
+  }
+
+  deleteImage(index: number) {
+    this.gridArray.splice(index, 1);
+    if (this._PostsService.post?.slides.length) this._PostsService.post.slides.splice(index, 1);
   }
 }
