ba5130606f9fad46cec86838091ff01be95a18e3
mfpuche <marii.puche@gmail.com>
2023-01-24 01:42:46 -0400

    task16

---
+++ src/app/shared/components/options-bar/options-bar.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Swiper, SwiperOptions } from 'swiper';
+
+@Component({
+  selector: 'app-options-bar',
+  templateUrl: './options-bar.component.html',
+  styleUrls: ['./options-bar.component.scss']
+})
+export class OptionsBarComponent implements OnInit {
+
+  @Input() options: Array<string> = [];
+  @Input() type: string = "1"
+  @Input() optCol: number = 0;
+  @Input() swiperMode: boolean = false;
+  @Output() selectedIndex = new EventEmitter();
+
+  selected: number = 0
+
+  swiperConfig: SwiperOptions = {
+    slidesPerView:'auto',
+    freeMode: true,
+    spaceBetween: 8,
+  };
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  selectOpt(index: number){
+    if(index!= this.selected){
+      this.selected = index;
+    }
+    this.selectedIndex.emit(this.selected);
+  }
+
+}
