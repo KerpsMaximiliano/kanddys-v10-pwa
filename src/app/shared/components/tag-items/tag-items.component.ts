cbf164abc05f5017dde4ab6c8295ba4affa63b26
Diego <diegodlopezg@gmail.com>
2023-01-27 12:40:02 -0400

    Fixes

--- src/app/shared/components/tag-items/tag-items.component.ts
+++ src/app/shared/components/tag-items/tag-items.component.ts
@@ -8,29 +8,29 @@ import { Button } from '../general-item/general-item.component';
 @Component({
   selector: 'app-tag-items',
   templateUrl: './tag-items.component.html',
-  styleUrls: ['./tag-items.component.scss']
+  styleUrls: ['./tag-items.component.scss'],
 })
 export class TagItemsComponent implements OnInit {
-
   environment: string = environment.assetsUrl;
   optional = true;
 
-  items: Array<any> = []
-  optionsButton: Array <Button> = []
+  items: Array<any> = [];
+  optionsButton: Array<Button> = [];
 
-  constructor(private itemsService: ItemsService) { }
+  constructor(private itemsService: ItemsService) {}
 
- async ngOnInit(){
-   let item = await this.itemsService.item('63c61f50a6ce9322ca216714').then(item=>{
-    for (let index = 0; index < 6; index++) {
-      this.items.push(item)
-      this.optionsButton.push({
-        clickEvent: (params: Tag) => {
-          alert("clicked")
-        },
-      })
-    }
-   });
+  async ngOnInit() {
+    let item = await this.itemsService
+      .item('63c61f50a6ce9322ca216714')
+      .then((item) => {
+        for (let index = 0; index < 6; index++) {
+          this.items.push(item);
+          this.optionsButton.push({
+            clickEvent: (params: Tag) => {
+              alert('clicked');
+            },
+          });
+        }
+      });
   }
-
 }
