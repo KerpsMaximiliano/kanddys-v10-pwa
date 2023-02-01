262e02930e3e2069ac797b0681121b9168d69c76
Diego <diegodlopezg@gmail.com>
2023-02-01 10:19:11 -0400

    Fixes

--- src/app/modules/admin/pages/admin-dashboard/admin-dashboard.component.ts
+++ src/app/modules/admin/pages/admin-dashboard/admin-dashboard.component.ts
@@ -6,43 +6,36 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.scss']
+  styleUrls: ['./admin-dashboard.component.scss'],
 })
 export class AdminDashboardComponent implements OnInit {
-
   environment: string = environment.assetsUrl;
-  artID: Array<string> = 
-  [
-    "63d429d7849f894c1895c659",
-    "63d420a8849f894c189544d4",
-    "63c93768a6ce9322ca278888",
-    "63c61f50a6ce9322ca216714"
-  ]
-  articulos: Array<any> = []
-
-  tags: Array<any> = 
-  [
-    {title:'articulos', dotsIcon: true},
-    {title:'categorias', dotsIcon: true}, 
-    {title:'colecciones', dotsIcon: true}
+  artID: Array<string> = [
+    '63d429d7849f894c1895c659',
+    '63d420a8849f894c189544d4',
+    '63c93768a6ce9322ca278888',
+    '63c61f50a6ce9322ca216714',
   ];
-  selected: number = 0
+  articulos: Array<any> = [];
 
-  constructor(private itemsService: ItemsService) { }
+  tags: Array<any> = [
+    { title: 'articulos', dotsIcon: true },
+    { title: 'categorias', dotsIcon: true },
+    { title: 'colecciones', dotsIcon: true },
+  ];
+  selected: number = 0;
 
-  async ngOnInit(){
-    
-    this.artID.forEach(async element => {
-      let item = await this.itemsService.item(element)
-      this.articulos
-  .push(item);
-      
+  constructor(private itemsService: ItemsService) {}
+
+  async ngOnInit() {
+    this.artID.forEach(async (element) => {
+      let item = await this.itemsService.item(element);
+      this.articulos.push(item);
     });
-    
   }
 
-  selectTag(index: number){
-    if(index!= this.selected){
+  selectTag(index: number) {
+    if (index != this.selected) {
       this.selected = index;
     }
   }
