f7126fea82fa462b17568a6fd38900d4ee281bb6
GabrielSR07 <GabrielSR07@users.noreply.github.com>
2023-02-08 19:36:41 -0400

    menu-button fixes

--- src/app/shared/components/menu-button/menu-button.component.ts
+++ src/app/shared/components/menu-button/menu-button.component.ts
@@ -9,6 +9,7 @@ import { environment } from 'src/environments/environment';
 })
 export class MenuButtonComponent implements OnInit {
   uri: string = environment.uri;
+  env: string = environment.assetsUrl;
   @Input() mode: string = 'basic';
   @Input() phone: string;
   @Input() link: string;
