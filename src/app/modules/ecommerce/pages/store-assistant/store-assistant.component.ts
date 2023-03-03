52d6c199199ae1e2ec82a691c880e6b7023942d1
Luis Barboza <lbarbozanav@gmail.com>
2023-03-03 14:47:58 -0400

    started markup

---
+++ src/app/modules/ecommerce/pages/store-assistant/store-assistant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-store-assistant',
+  templateUrl: './store-assistant.component.html',
+  styleUrls: ['./store-assistant.component.scss']
+})
+export class StoreAssistantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
