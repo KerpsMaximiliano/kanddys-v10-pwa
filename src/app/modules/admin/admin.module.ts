4e933daeaf54e59349e968a64b5d15bc8724fdf6
Ezequiel Chain <ezequielechain@gmail.com>
2023-07-08 12:01:44 -0300

    added item-selector and supplier-register

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -73,6 +73,8 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { createTranslateLoader } from 'src/app/core/functions/create-translate-loader';
 import { HttpClient } from '@angular/common/http';
 import { ItemsSlidesEditorComponent } from 'src/app/shared/components/items-slides-editor/items-slides-editor.component';
+import { ItemSelectorComponent } from './pages/item-selector/item-selector.component';
+import { SupplierRegisterComponent } from './pages/supplier-register/supplier-register.component';
 
 const routes: Routes = [
   { path: 'create-item', redirectTo: 'create-article', pathMatch: 'full' },
@@ -459,6 +461,14 @@ const routes: Routes = [
     path: 'merchants-entry',
     component: MerchantsEntryComponent,
   },
+  {
+    path: 'item-selector',
+    component: ItemSelectorComponent,
+  },
+  {
+    path: 'supplier-register',
+    component: SupplierRegisterComponent,
+  },
 ];
 
 @NgModule({
@@ -513,6 +523,8 @@ const routes: Routes = [
     RewardsDisplayComponent,
     MerchantsEntryComponent,
     ItemCreationComponent,
+    ItemSelectorComponent,
+    SupplierRegisterComponent,
   ],
   exports: [ArticleCreatorComponent],
   imports: [
