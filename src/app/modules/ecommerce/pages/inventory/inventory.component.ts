6df878619d8ca0f422889c1aa0415c84a4da7df1
Jorge Chourio <jorge_chourio@hotmail.com>
2023-07-11 01:07:49 -0400

    Progress

--- src/app/modules/ecommerce/pages/inventory/inventory.component.ts
+++ src/app/modules/ecommerce/pages/inventory/inventory.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Merchant } from 'src/app/core/models/merchant';
+import { Quotation } from 'src/app/core/models/quotations';
+import { MerchantsService } from 'src/app/core/services/merchants.service';
+import { QuotationsService } from 'src/app/core/services/quotations.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-inventory',
@@ -7,9 +12,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryComponent implements OnInit {
 
-  constructor() { }
+  env: string = environment.assetsUrl;
 
-  ngOnInit(): void {
+  openNavigation: boolean = false;
+
+  merchant: Merchant;
+  quotations: Quotation[] = [];
+
+  constructor(
+    private merchantsService: MerchantsService,
+    private quotationsService: QuotationsService
+  ) { }
+
+  async ngOnInit() {
+    await this.getMerchantDefault();
+    await this.getQuotations(this.merchant._id);
+  }
+
+  async getMerchantDefault() {
+    try {
+      const result = await this.merchantsService.merchantDefault();
+      this.merchant = result;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getQuotations(merchantId: string) {
+    try {
+      const result = await this.quotationsService.quotations(
+        {
+          findBy: {
+            merchant: merchantId
+          },
+          options: {
+            limit: -1,
+            sortBy: "createdAt:desc"
+          }
+        },
+      );
+      this.quotations = result;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 }
