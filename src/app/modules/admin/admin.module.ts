d280fc14aef6150a36009186677f061796f0e1d6
Jorge Chourio <jorge_chourio@hotmail.com>
2023-07-11 03:13:29 -0400

    Integrations to inventory component

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -393,7 +393,7 @@ const routes: Routes = [
         component: ItemSelectorComponent,
       },
       {
-        path: 'quotes',
+        path: 'quotations',
         component: InventoryComponent,
       }
     ],
