dbd507ec2b51d16649d74b9b9fcfe9c0bed96bb4
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2023-07-28 07:25:09 -0400

    feat: integration of form in user-entry page

--- src/app/modules/admin/pages/user-entry/user-entry.component.ts
+++ src/app/modules/admin/pages/user-entry/user-entry.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { MerchantsService } from 'src/app/core/services/merchants.service';
 
 @Component({
   selector: 'app-user-entry',
   templateUrl: './user-entry.component.html',
-  styleUrls: ['./user-entry.component.scss']
+  styleUrls: ['./user-entry.component.scss'],
 })
 export class UserEntryComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private merchantsService: MerchantsService
+  ) {}
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      phone: ['', Validators.required],
+      name: ['', Validators.required],
+      email: [''],
+      clientOfMerchants: [''],
+    });
   }
 
+  /**
+   * Envía el formulario de registro de usuario.
+   *
+   * @function
+   * @async
+   * @returns {Promise<void>} Una promesa que se resuelve cuando se completa el registro.
+   */
+  async sendForm(): Promise<void> {
+    if (!this.form.invalid) {
+      const merchantDefault = await this.merchantsService.merchantDefault();
+
+      const { phone, name, clientOfMerchants } = this.form.value;
+      const inputData = { phone, name, clientOfMerchants };
+      inputData.clientOfMerchants = [merchantDefault._id];
+
+      const data = await this.authService.signup(inputData, '');
+      console.log(data);
+    }
+  }
 }
