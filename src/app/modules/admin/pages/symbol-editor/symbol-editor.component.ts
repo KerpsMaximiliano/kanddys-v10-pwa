d00c0cad5d165b960698cd33c0d58d2e43acf60e
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-08-15 15:30:33 -0300

    Call required services

--- src/app/modules/admin/pages/symbol-editor/symbol-editor.component.ts
+++ src/app/modules/admin/pages/symbol-editor/symbol-editor.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MerchantsService } from 'src/app/core/services/merchants.service';
+import { PostsService } from 'src/app/core/services/posts.service';
 
 @Component({
   selector: 'app-symbol-editor',
@@ -10,9 +12,31 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 export class SymbolEditorComponent implements OnInit {
   env : string = environment.assetsUrl;
   toggleSwitch : boolean = false;
-  constructor() {}
+  postSolidary : any;
+  postImages : any[];
+  constructor(
+    private MerchantsService: MerchantsService,
+    private PostsService: PostsService
+    ) {}
 
   ngOnInit(): void {
+    this.getMerchantFunctionality()
   }
 
+  async getMerchantFunctionality() {
+    let merchantId : string;
+    await this.MerchantsService.merchantDefault().then((res) => {
+      console.log(res)
+      merchantId = res._id;
+    });
+    await this.MerchantsService.merchantFuncionality(merchantId).then((res) => {
+      console.log(res)
+      this.postSolidary = res.postSolidary;
+      this.toggleSwitch = this.postSolidary.active;
+    })
+    this.PostsService.slidesByPost(this.postSolidary.post._id).then((res) => {
+      console.log(res)
+      this.postImages = res;
+    })
+  }
 }
