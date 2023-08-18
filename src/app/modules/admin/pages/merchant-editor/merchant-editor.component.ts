997aedfec6892cb8b49d6d2d7a7463a4c4a3637f
Matias Rivero <riveromatias82@gmail.com>
2023-08-18 18:25:16 -0300

    merchant editor integration

--- src/app/modules/admin/pages/merchant-editor/merchant-editor.component.ts
+++ src/app/modules/admin/pages/merchant-editor/merchant-editor.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { MerchantsService } from 'src/app/core/services/merchants.service';
+import { MerchantInput } from 'src/app/core/models/merchant';
+import { base64ToFile } from 'src/app/core/helpers/files.helpers';
 
 @Component({
   selector: 'app-merchant-editor',
@@ -6,10 +10,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./merchant-editor.component.scss']
 })
 export class MerchantEditorComponent implements OnInit {
+  email: string | null = null;
+  image: string | null = null;
+  imageName: string | null = null;
+  imageBase64: string | null = null;
+  name: string | null = null;
+  slug: string | null = null;
+  merchantId: string | null = null;
 
-  constructor() { }
+  constructor(
+    private merchantsService: MerchantsService,
+    private authService: AuthService,
+  ) {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const user = await this.authService.me();
+    if (user && user?._id) {
+      try {
+        const merchant = await this.merchantsService.merchantDefault(user._id);
+        if (merchant && merchant?._id) {
+          if (merchant?.owner?.email) this.email = merchant?.owner?.email;
+          this.merchantId = merchant._id;
+          this.name = merchant.name;
+          this.slug = merchant.slug;
+          this.image = merchant.image;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 
+  addField(field) {
+    this[field] = '';
+  }
+
+  onFileUpload(event: any) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.imageBase64 = e.target.result as string
+    };
+    reader.readAsDataURL(event.target.files[0] as File);
+    this.imageName = event.target.files[0].name as string;
+  }
+
+  async save() {
+    if (this.email) {
+      try {
+        await this.authService.updateMe({email: this.email});
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    if (this.name || this.slug) {
+      try {
+        let merchantInput = {};
+        if(this.name) merchantInput['name'] = this.name;
+        if(this.slug) merchantInput['slug'] = this.slug;
+        if(this.imageName) merchantInput['image'] = this.imageName;
+        await this.merchantsService.updateMerchant(
+          merchantInput,
+          this.merchantId,
+          this.imageBase64? [base64ToFile(this.imageBase64)]: null,
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 }
