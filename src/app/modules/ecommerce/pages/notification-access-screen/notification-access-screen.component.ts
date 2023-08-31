ea7ed6a4950e04a46d9d7298ba16960ec050a486
Danilo95415 <danilo95415@gmail.com>
2023-08-30 17:16:19 -0700

    create three components

---
+++ src/app/modules/ecommerce/pages/notification-access-screen/notification-access-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-access-screen',
+  templateUrl: './notification-access-screen.component.html',
+  styleUrls: ['./notification-access-screen.component.scss']
+})
+export class NotificationAccessScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
