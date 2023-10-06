ec1b5a22ca6435d0cf0c57fb92008aee66a5db0d
Luis Barboza <lbarbozanav@gmail.com>
2023-10-05 20:10:16 -0400

    added memories-management page

--- src/app/modules/ecommerce/pages/dalia-training/dalia-training.component.ts
+++ src/app/modules/ecommerce/pages/dalia-training/dalia-training.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { lockUI, unlockUI } from 'src/app/core/helpers/ui.helpers';
@@ -8,6 +9,10 @@ import { HeaderService } from 'src/app/core/services/header.service';
 import { MerchantsService } from 'src/app/core/services/merchants.service';
 import { SaleFlowService } from 'src/app/core/services/saleflow.service';
 import { DialogService } from 'src/app/libs/dialog/services/dialog.service';
+import {
+  FormComponent,
+  FormData,
+} from 'src/app/shared/dialogs/form/form.component';
 import { GeneralFormSubmissionDialogComponent } from 'src/app/shared/dialogs/general-form-submission-dialog/general-form-submission-dialog.component';
 import { environment } from 'src/environments/environment';
 
@@ -40,125 +45,171 @@ export class DaliaTrainingComponent implements OnInit, OnDestroy {
   timeoutCutKey: any = null;
   showLogin: boolean = false;
   loginData = {
-    redirectionRoute: '/ecommerce/daliah-training',
+    redirectionRoute: '/ecommerce/laia-training',
     redirectionRouteId: null,
     entity: 'MerchantAccess',
     jsondata: '',
   };
+  memoryName: string = null;
   queryParamsSubscription: Subscription;
+  routeParamsSubscription: Subscription;
   memoryTextareaValueChangeSubscription: Subscription;
+  memoryNameDialogSubscription: Subscription;
+  vectorId: string = null;
 
   constructor(
     private gptService: Gpt3Service,
     public headerService: HeaderService,
     private dialog: DialogService,
+    private matDialog: MatDialog,
     private router: Router,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.queryParamsSubscription = this.route.queryParams.subscribe(
-      async ({ jsondata }) => {
-        const textarea: HTMLElement = document.querySelector('.base-text');
+    this.routeParamsSubscription = this.route.params.subscribe(
+      async ({ vectorId }) => {
+        this.vectorId = vectorId ? vectorId : null;
 
-        textarea.addEventListener('input', () => {
-          console.log('textarea scrollHeight', textarea.scrollHeight);
-          if (textarea.scrollHeight > 171) {
-            if (this.showExtendButton === false) {
-              this.showExtendButton = true;
-            }
+        this.queryParamsSubscription = this.route.queryParams.subscribe(
+          async ({ jsondata }) => {
+            const textarea: HTMLElement = document.querySelector('.base-text');
 
-            if (this.alreadyClickedShowButton) {
-              textarea.style.height = 'auto'; // Reset height to auto
-              textarea.style.height = textarea.scrollHeight + 'px'; // Set the new height based on content
-              this.showExtendButton = false;
-            }
-            this.passedTextLimit = true;
-          } else {
-            this.showExtendButton = false;
-
-            if (this.passedTextLimit) {
-              textarea.style.height = '171px';
-            }
-          }
-        });
-
-        document.addEventListener('keydown', (event: KeyboardEvent) => {
-          const targetElement: HTMLElement = event.target as HTMLElement;
-
-          if (
-            (event.key === 'Delete' || event.key === 'Backspace') &&
-            this.passedTextLimit &&
-            targetElement.classList.contains('base-text') &&
-            textarea.scrollHeight <= 171
-          ) {
-            textarea.style.height = '171px';
-            this.showExtendButton = false;
-            this.alreadyClickedShowButton = false;
-          } else if (
-            (event.key === 'Delete' || event.key === 'Backspace') &&
-            this.passedTextLimit &&
-            targetElement.classList.contains('base-text') &&
-            textarea.scrollHeight >= 171
-          ) {
-            if (!this.timeoutDeleteKey)
-              this.timeoutDeleteKey = setTimeout(() => {
-                if (textarea.scrollHeight <= 171) {
-                  textarea.style.height = '171px';
-                  this.showExtendButton = false;
-                  this.alreadyClickedShowButton = false;
+            textarea.addEventListener('input', () => {
+              console.log('textarea scrollHeight', textarea.scrollHeight);
+              if (textarea.scrollHeight > 171) {
+                if (this.showExtendButton === false) {
+                  this.showExtendButton = true;
                 }
 
-                clearTimeout(this.timeoutDeleteKey);
-              }, 400);
-          }
+                if (this.alreadyClickedShowButton) {
+                  textarea.style.height = 'auto'; // Reset height to auto
+                  textarea.style.height = textarea.scrollHeight + 'px'; // Set the new height based on content
+                  this.showExtendButton = false;
+                }
+                this.passedTextLimit = true;
+              } else {
+                this.showExtendButton = false;
 
-          if (
-            event.ctrlKey &&
-            (event.key === 'x' || event.key === 'X') &&
-            this.passedTextLimit &&
-            targetElement.classList.contains('base-text')
-          ) {
-            // Your code to handle Ctrl + X here
-            // Prevent the default behavior (cut action) if needed
+                if (this.passedTextLimit) {
+                  textarea.style.height = '171px';
+                }
+              }
+            });
 
-            textarea.style.height = '171px';
-            this.showExtendButton = false;
-            this.alreadyClickedShowButton = false;
+            document.addEventListener('keydown', (event: KeyboardEvent) => {
+              const targetElement: HTMLElement = event.target as HTMLElement;
 
-            if (!this.timeoutCutKey) {
-              this.timeoutCutKey = setTimeout(() => {
+              if (
+                (event.key === 'Delete' || event.key === 'Backspace') &&
+                this.passedTextLimit &&
+                targetElement.classList.contains('base-text') &&
+                textarea.scrollHeight <= 171
+              ) {
                 textarea.style.height = '171px';
                 this.showExtendButton = false;
                 this.alreadyClickedShowButton = false;
-              }, 400);
-            }
-            event.preventDefault();
-          }
-        });
+              } else if (
+                (event.key === 'Delete' || event.key === 'Backspace') &&
+                this.passedTextLimit &&
+                targetElement.classList.contains('base-text') &&
+                textarea.scrollHeight >= 171
+              ) {
+                if (!this.timeoutDeleteKey)
+                  this.timeoutDeleteKey = setTimeout(() => {
+                    if (textarea.scrollHeight <= 171) {
+                      textarea.style.height = '171px';
+                      this.showExtendButton = false;
+                      this.alreadyClickedShowButton = false;
+                    }
 
-        this.memoryTextareaValueChangeSubscription = this.form
-          .get('memory')
-          .valueChanges.subscribe((change) => {
-            this.loginData.jsondata = JSON.stringify({
-              memoryToSave: this.form.get('memory').value,
+                    clearTimeout(this.timeoutDeleteKey);
+                  }, 400);
+              }
+
+              if (
+                event.ctrlKey &&
+                (event.key === 'x' || event.key === 'X') &&
+                this.passedTextLimit &&
+                targetElement.classList.contains('base-text')
+              ) {
+                // Your code to handle Ctrl + X here
+                // Prevent the default behavior (cut action) if needed
+
+                textarea.style.height = '171px';
+                this.showExtendButton = false;
+                this.alreadyClickedShowButton = false;
+
+                if (!this.timeoutCutKey) {
+                  this.timeoutCutKey = setTimeout(() => {
+                    textarea.style.height = '171px';
+                    this.showExtendButton = false;
+                    this.alreadyClickedShowButton = false;
+                  }, 400);
+                }
+                event.preventDefault();
+              }
             });
-          });
 
-        if (jsondata) {
-          let parsedData = JSON.parse(decodeURIComponent(jsondata));
+            this.memoryTextareaValueChangeSubscription = this.form
+              .get('memory')
+              .valueChanges.subscribe((change) => {
+                this.loginData.jsondata = JSON.stringify({
+                  memoryToSave: this.form.get('memory').value,
+                });
+              });
 
-          if (parsedData.memoryToSave) {
-            this.form.get('memory').setValue(parsedData.memoryToSave);
-            await this.saveMemoryInKnowledgeBase();
+            if (jsondata) {
+              let parsedData = JSON.parse(decodeURIComponent(jsondata));
 
-            const urlWithoutQueryParams = this.router.url.split('?')[0];
+              if (parsedData.memoryToSave) {
+                this.form.get('memory').setValue(parsedData.memoryToSave);
+                await this.saveMemoryInKnowledgeBase();
 
-            window.history.replaceState({}, 'SaleFlow', urlWithoutQueryParams);
+                const urlWithoutQueryParams = this.router.url.split('?')[0];
+
+                window.history.replaceState(
+                  {},
+                  'SaleFlow',
+                  urlWithoutQueryParams
+                );
+              }
+            }
+
+            if (this.vectorId) {
+              await this.loadVectorData();
+            }
           }
-        }
+        );
       }
     );
+  }
+
+  async loadVectorData() {
+    try {
+      lockUI();
+
+      const vectorData = await this.gptService.getVectorByIdInKnowledgeBase(
+        this.vectorId
+      );
+
+      if (vectorData?.length) {
+        this.form.get('memory').setValue(vectorData[0].text);
+        this.memoryName = vectorData[0].name ? vectorData[0].name : null;
+
+        setTimeout(() => {
+          const textarea: HTMLElement = document.querySelector('.base-text');
+          const event = new Event('input');
+
+          textarea.dispatchEvent(event);
+        }, 300);
+      }
+
+      unlockUI();
+    } catch (error) {
+      unlockUI();
+      console.error(error);
+    }
   }
 
   showMoreText() {
@@ -271,22 +322,44 @@ export class DaliaTrainingComponent implements OnInit, OnDestroy {
 
       try {
         await this.headerService.checkIfUserIsAMerchantAndFetchItsData();
-        const dataFeeded = await this.gptService.feedKnowledgeBaseWithTextData(
-          this.form.get('memory').value
-        );
 
-        if (dataFeeded) {
-          this.dialog.open(GeneralFormSubmissionDialogComponent, {
-            type: 'centralized-fullscreen',
-            props: {
-              message: 'Se ha registrado la memoria en laia exitosamente',
-              icon: dataFeeded ? 'check-circle.svg' : 'sadFace.svg',
-              showCloseButton: dataFeeded ? false : true,
-            },
-            customClass: 'app-dialog',
-            flags: ['no-header'],
-          });
+        let success = false;
+
+        if (!this.vectorId) {
+          const dataFeeded =
+            await this.gptService.feedKnowledgeBaseWithTextData(
+              this.form.get('memory').value,
+              this.memoryName
+            );
+
+          success = dataFeeded ? true : false;
+        } else {
+          const vectorInserted =
+            await this.gptService.updateVectorInKnowledgeBase(
+              this.vectorId,
+              this.form.get('memory').value,
+              this.memoryName
+            );
+
+          if (vectorInserted?.vector) {
+            this.vectorId = vectorInserted.vector.id;
+            success = true;
+          }
         }
+
+        this.dialog.open(GeneralFormSubmissionDialogComponent, {
+          type: 'centralized-fullscreen',
+          props: {
+            message: success
+              ? 'Se ha registrado la memoria en laia exitosamente'
+              : 'Ocurrió un error inesperado',
+            icon: success ? 'check-circle.svg' : 'sadFace.svg',
+            showCloseButton: success ? false : true,
+          },
+          customClass: 'app-dialog',
+          flags: ['no-header'],
+        });
+
         unlockUI();
       } catch (error) {
         console.error(error);
@@ -314,15 +387,48 @@ export class DaliaTrainingComponent implements OnInit, OnDestroy {
   }
 
   back() {
+    if (this.vectorId) {
+      return this.router.navigate(['/ecommerce/laia-memories-management']);
+    }
+
     return this.router.navigate(['/ecommerce/club-landing'], {
       queryParams: {
-        tabarIndex: 2
-      }
+        tabarIndex: 2,
+      },
     });
   }
 
+  editMemoryName = () => {
+    let fieldsToCreate: FormData = {
+      fields: [
+        {
+          label: 'Nombre de la memoria',
+          name: 'memoryName',
+          type: 'text',
+          validators: [Validators.pattern(/[\S]/), Validators.required],
+        },
+      ],
+      automaticallyFocusFirstField: true,
+    };
+
+    const dialogRef = this.matDialog.open(FormComponent, {
+      data: fieldsToCreate,
+    });
+
+    this.memoryNameDialogSubscription = dialogRef
+      .afterClosed()
+      .subscribe((result: FormGroup) => {
+        if (result?.value['memoryName']) {
+          this.memoryName = result?.value['memoryName'];
+          console.log('Nombre de la memoria', result?.value['memoryName']);
+        }
+      });
+  };
+
   ngOnDestroy() {
+    this.routeParamsSubscription.unsubscribe();
     this.queryParamsSubscription.unsubscribe();
     this.memoryTextareaValueChangeSubscription.unsubscribe();
+    this.memoryNameDialogSubscription?.unsubscribe();
   }
 }
