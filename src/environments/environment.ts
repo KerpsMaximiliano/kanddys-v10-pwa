79428b1cddc27377a5f63eb895a5b96a5c88f05a
Jorge Chourio <jorge_chourio@hotmail.com>
2023-10-10 01:32:11 -0400

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@ export const environment = {
   api: {
     // url: 'https://api.rewardcharly.com',
     // url: 'http://165.227.28.4:3000',
-    url: 'http://localhost:3500',
-    // url: 'https://api.kanddys.com',
+    // url: 'http://localhost:3500',
+    url: 'https://api.kanddys.com',
     key: 'a6c6d9880190ad2c4d477b89b44107b82b3e4902f293fe710d9a904de283f8f7',
   },
   chatAPI: {
-    url: 'http://localhost:8000',
+    url: 'http://143.198.72.59:8000',
     key: '2496a588184b9dc9e4f436b48cad0c36274e625d5b490739d7285a07a578e895'
   },
   APIREST: 'https://appointments.kanddys.com/v1',
