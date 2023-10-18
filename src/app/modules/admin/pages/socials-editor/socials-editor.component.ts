bc99dbcf3ae00a5e953420fa78f883b4b55cec7a
Matias Rivero <riveromatias82@gmail.com>
2023-10-18 11:04:15 -0300

    socials editor integration

--- src/app/modules/admin/pages/socials-editor/socials-editor.component.ts
+++ src/app/modules/admin/pages/socials-editor/socials-editor.component.ts
@@ -3,6 +3,8 @@ import { Location } from '@angular/common';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { MerchantsService } from 'src/app/core/services/merchants.service';
 import { ContactService } from 'src/app/core/services/contact.service';
+import { isURL } from 'src/app/core/helpers/strings.helpers';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-socials-editor',
@@ -36,12 +38,14 @@ export class SocialsEditorComponent implements OnInit {
 
   contactDefault: any | null = null;
   merchantId: string | null = null;
+  error: boolean = false;
 
   constructor(
     private authService: AuthService,
     private merchantsService: MerchantsService,
     private contactService: ContactService,
     private location: Location,
+    private snackbar: MatSnackBar,
   ) { }
 
   async ngOnInit(): Promise<void> {
@@ -75,24 +79,36 @@ export class SocialsEditorComponent implements OnInit {
       this.contactDefault = newContact;
       await this.contactService.contactSetDefault(this.contactDefault._id);
     }
+    this.error = false;
     for (const prop in this.links) {
       if (this[`link${prop}`] && this[`link${prop}`].length > 0) {
-        const linkAdded = await this.contactService.contactAddLink(
-          {
-            name: prop,
-            value: this[`link${prop}`],
-          },
-          this.contactDefault._id
-        );
+        if (isURL(this[`link${prop}`])) {
+          try {
+            const linkAdded = await this.contactService.contactAddLink(
+              {
+                name: prop,
+                value: this[`link${prop}`],
+              },
+              this.contactDefault._id
+            );
+          } catch (error) {
+            console.log(error);
+          } 
+        } else {
+          this.error = true;
+          this.snackbar.open('URL inválida', 'Cerrar', {
+            duration: 3000,
+          });
+        }
       }
     }
-    this.location.back();
+    if (!this.error) this.location.back();
   }
 
   async removeLink(option: string) {
-    this.links[option] = '';
     try {
       const linkRemoved = await this.contactService.contactRemoveLink(this.linksIds[option], this.contactDefault._id);
+      this.links[option] = '';
     } catch (error) {
       console.log(error);
     }
